9a95f6517641718e226c9658b76d6d14ba4fbd3a
= <=>
2023-09-28 12:03:38 +0200

    FEAT: Added heatmap and neat agregates of mean.

--- data/helper.py
+++ data/helper.py
@@ -2,12 +2,12 @@ from datetime import datetime, timedelta
 
 def find_missing_dates(dates: list[datetime], hours: int, mins: int)->list[datetime]: 
 
-    datetime_obj = dates[0]
+    datetime_obj = dates.iloc[0]
     missing_dates = []
 
     count = 0
-    while count < len(dates):
-        if dates[count] != datetime_obj:
+    while dates.iloc[count] != dates.iloc[-1]:
+        if dates.iloc[count] != datetime_obj:
             missing_dates.append(datetime_obj)
         else:
             count += 1
