b218c9f9e65895a54b59e860f38361a83b5eac64
Isak Herrera <isakasa@stud.ntnu.no>
2023-10-03 11:04:14 +0200

    FEAT: Added nice function for plotting and getting mean values.

--- data/helper.py
+++ data/helper.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta
+import pandas as pd
+import seaborn as sns
+import matplotlib.pylab as plt
 
 def find_missing_dates(dates: list[datetime], hours: int, mins: int)->list[datetime]: 
 
@@ -13,3 +16,18 @@ def find_missing_dates(dates: list[datetime], hours: int, mins: int)->list[datet
             count += 1
         datetime_obj += timedelta(hours= hours, minutes = mins)
     return missing_dates
+
+def reformat_time(df_missing_format: pd.DataFrame, df_desired_format: pd.DataFrame) -> pd.DataFrame:
+    df_missing_format["date_forecast"] = pd.to_datetime(df_missing_format["date_forecast"])
+    df = df_missing_format.groupby([df_missing_format["date_forecast"].dt.hour]).mean()
+    df["date_forecast"] = df_desired_format["time"]
+    return df
+
+def corr_mat(dataFrame_1: pd.DataFrame, dataFrame_2: pd.DataFrame, title: str)-> None:
+    dataFrame_1["pv_measurement"] = dataFrame_2["pv_measurement"]
+
+    corr_matrix = dataFrame_1.corr()
+
+    sns.heatmap(corr_matrix, cmap="PiYG")
+    plt.title(title)
+    plt.show()
