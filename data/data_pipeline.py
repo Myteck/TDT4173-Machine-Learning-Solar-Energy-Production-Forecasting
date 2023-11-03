458419e01656faceb777ae6db392efe3245cbe63
= <=>
2023-11-03 16:05:12 +0100

    FEAT: Added

--- data/data_pipeline.py
+++ data/data_pipeline.py
@@ -6,11 +6,10 @@ import feature_generation as feat_gen
 import data_processing as dat_proc
 import pandas as pd
 
-def train_data_processing(X: pd.DataFrame, y: pd.DataFrame, filter: list[str], add_y_signal: bool = False):
+def train_data_processing(X: pd.DataFrame, y: pd.DataFrame, filter_list: list[str] = [], add_y_signal: bool = False):
    
     # Removing NaN values. If there are missing values treat start and end points as beginning and end of a line.
-    X = X.interpolate(method='linear')
-    X = X.bfill()
+    X = X.interpolate(method='linear', limit_direction = "both")
 
     # Extract necesarry values for feature generation.
     timestamps = "date_forecast"
@@ -25,12 +24,10 @@ def train_data_processing(X: pd.DataFrame, y: pd.DataFrame, filter: list[str], a
     time_df = feat_gen.time_data_from_df(X, timestamps)
 
 
-    X = pd.concat([X, der_df, dder_df, int_df, dint_df, time_df], axis = "columns")
+    X = pd.concat([X, der_df, dder_df, dint_df, int_df, time_df], axis = "columns")
 
-    if len(filter) > 0:
-        X = X[filter + ["date_forecast"]]
-
-    y = train_a.dropna()
+    if len(filter_list) > 0:
+        X = X[filter_list + ["date_forecast"]]
 
     # Additional features
     der_y = feat_gen.difference_df(y, "time", ["pv_measurement"])
@@ -48,50 +45,54 @@ def train_data_processing(X: pd.DataFrame, y: pd.DataFrame, filter: list[str], a
     y = y_BIG["pv_measurement"]
     y = y.reset_index(drop = True)
     
-    # Removing datetime object column
-    X = X.reset_index(drop = True)
-    X = X.drop(timestamps, axis=1)
     
     if add_y_signal:
         # Removing datetime object column.
         y_features = y_BIG.drop('pv_measurement', axis=1)
         y_features = y_features.drop('time', axis=1)
         y_features = y_features.reset_index(drop = True)
+
+        X = pd.concat([X, y_features], axis = 1)
     
-    
+    # Removing datetime object column
+    X = X.drop(timestamps, axis=1)
+    X = X.reset_index(drop = True)
 
     return X, y
 
-def pred_data_processing(df: pd.DataFrame, filter: list[str] = []) -> pd.DataFrame:
+def pred_data_processing(X_pred: pd.DataFrame, filter_list: list[str] = []) -> pd.DataFrame:
     """
     A function that reads
     """
     
     # Removing NaN values. If there are missing values treat start and end points as beginning and end of a line.
-    df = df.interpolate(method = 'linear', limit_direction = "both")
+    X_pred = X_pred.interpolate(method = 'linear')
+    X_pred = X_pred.bfill()
 
     # Extract necesarry values for feature generation.
     timestamps = "date_forecast"
 
     # Removing date-time from measurements
-    measurements = list(df.columns.values)
+    measurements = list(X_pred.columns.values)
     measurements.remove("date_forecast")
     measurements.remove("date_calc")
 
     # Probable features that may be used
-    der_df = feat_gen.difference_df(df, timestamps, measurements)
-    dder_df = feat_gen.double_derivative_from_df(df, timestamps, measurements)
-    int_df = feat_gen.daily_accumulated_val_df(df, timestamps, measurements)
-    dint_df = feat_gen.daily_accumulated_val_squared_df(df, timestamps, measurements)
-    time_df = feat_gen.time_data_from_df(df, timestamps)
+    der_df = feat_gen.difference_df(X_pred, timestamps, measurements)
+    dder_df = feat_gen.double_derivative_from_df(X_pred, timestamps, measurements)
+    int_df = feat_gen.daily_accumulated_val_df(X_pred, timestamps, measurements)
+    dint_df = feat_gen.daily_accumulated_val_squared_df(X_pred, timestamps, measurements)
+    time_df = feat_gen.time_data_from_df(X_pred, timestamps)
 
-    new_df = pd.concat([df, der_df, dder_df, int_df, dint_df, time_df], axis = "columns")
+    
 
-    if len(filter) > 0:
-        new_df = new_df[filter]
+    X_pred_new = pd.concat([X_pred, der_df, dder_df, int_df, dint_df, time_df], axis = "columns")
+
+    if len(filter_list) > 0:
+        X_pred_new = X_pred_new[filter_list]
     else:
-        new_df = new_df.drop("date_forecast", axis = 1)
-        new_df = new_df.drop("date_calc", axis = 1)
+        X_pred_new = X_pred_new.drop("date_forecast", axis = 1)
+        X_pred_new = X_pred_new.drop("date_calc", axis = 1)
 
-    return new_df
+    return X_pred_new
 
